3966435bb29b1edafbecc3e9216853094175caa6
RND2BMC <rnd2biometriccables@gmail.com>
2025-07-31 16:44:59 +0530

    log.js

--- api/log.js
+++ api/log.js
@@ -1,15 +1,22 @@
+// /api/log.js
+
 let latestO2 = 0;
 
-export default function handler(req, res) {
-  if (req.method === "POST") {
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
     const { o2 } = req.body;
-    latestO2 = o2;
-    return res.status(200).json({ message: "O2 stored", o2 });
-  }
 
-  if (req.method === "GET") {
-    return res.status(200).json({ o2: latestO2 });
+    if (typeof o2 === 'number') {
+      latestO2 = o2;
+      return res.status(200).json({ message: 'O2 stored', o2 });
+    } else {
+      return res.status(400).json({ error: 'Invalid O2 value' });
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    return res.status(405).end('Method Not Allowed');
   }
-
-  res.status(405).end(); // Method Not Allowed
 }
+
+// Export getter for other API routes
+export { latestO2 };
